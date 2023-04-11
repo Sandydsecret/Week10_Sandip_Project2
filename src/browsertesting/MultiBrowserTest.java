824c0d5e0595d95496730692e5da38e6b0614c0d
Sandydsecret <126623748+Sandydsecret@users.noreply.github.com>
2023-04-11 22:59:05 +0100

    Update MultiBrowserTest.java

--- src/browsertesting/MultiBrowserTest.java
+++ src/browsertesting/MultiBrowserTest.java
@@ -36,9 +36,9 @@ public class MultiBrowserTest {
         //Print the page source
         System.out.println("Page Source: "+driver.getPageSource());
         //Enter the email to email field
-        driver.findElement(By.name("username")).sendKeys("Maitriborad");
+        driver.findElement(By.name("username")).sendKeys("Sandydsewcret");
         //Enter the password to password field
-        driver.findElement(By.name("password")).sendKeys("Maitri@123");
+        driver.findElement(By.name("password")).sendKeys("Sandy123");
         //Close the browser
         //driver.close();
     }
